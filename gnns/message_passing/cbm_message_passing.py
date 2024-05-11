210b8fa3a62a190180e06a4886f2e2f427ffcfed
SamirMoustafa <samir.moustafa.97@gmail.com>
2024-05-11 02:41:17 +0200

    add normalization as option in cbm

--- gnns/message_passing/cbm_message_passing.py
+++ gnns/message_passing/cbm_message_passing.py
@@ -5,8 +5,9 @@ from gnns.message_passing.base_message_passing import MessagePassing
 
 
 class CBMSequentialMKLCSRSparseMatrixMessagePassing(MessagePassing):
-    def __init__(self, flow: str = "source_to_target", node_dim: int = 0, alpha: int = 2, cached: bool = True):
+    def __init__(self, flow: str = "source_to_target", node_dim: int = 0, alpha: int = 2, normalize: bool = True, cached: bool = True):
         super(CBMSequentialMKLCSRSparseMatrixMessagePassing, self).__init__(flow, node_dim)
+        self.normalize = normalize
         self.cached = cached
         self.alpha = alpha
         self.cached_a_t = None
@@ -21,17 +22,17 @@ class CBMSequentialMKLCSRSparseMatrixMessagePassing(MessagePassing):
         if self.cached:
             if self.cached_a_t is None:
                 edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=True, alpha=self.alpha)
+                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
             if self.cached_empty_tensor is None:
                 self.cached_empty_tensor = empty(size=(edge_index.max().item() + 1, x.size(1),), dtype=x.dtype, device=x.device)
             a_t = self.cached_a_t
             out = self.cached_empty_tensor
         else:
             edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=True, alpha=self.alpha)
+            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
             out = empty(size=(edge_index.max().item() + 1, x.size(1),), dtype=x.dtype, device=x.device)
 
-        a_t.seq_mkl_csr_fused_spmm_update(x, out)
+        a_t.seq_mkl_csr_fused_spmm_update(x, out) if self.normalize else a_t.seq_mkl_csr_spmm_update(x, out)
         return out
 
 
@@ -45,23 +46,24 @@ class CBMParallelMKLCSRSparseMatrixMessagePassing(CBMSequentialMKLCSRSparseMatri
         if self.cached:
             if self.cached_a_t is None:
                 edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=True, alpha=self.alpha)
+                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
             if self.cached_empty_tensor is None:
                 self.cached_empty_tensor = empty(size=(edge_index.max().item() + 1, x.size(1),), dtype=x.dtype, device=x.device)
             a_t = self.cached_a_t
             out = self.cached_empty_tensor
         else:
             edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=True, alpha=self.alpha)
+            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
             out = empty(size=(edge_index.max().item() + 1, x.size(1),), dtype=x.dtype, device=x.device)
 
-        a_t.omp_mkl_csr_fused_spmm_update(x, out)
+        a_t.omp_mkl_csr_fused_spmm_update(x, out) if self.normalize else a_t.omp_mkl_csr_spmm_update(x, out)
         return out
 
 
 class CBMSequentialTorchCSRSparseMatrixMessagePassing(MessagePassing):
-    def __init__(self, flow: str = "source_to_target", node_dim: int = 0, alpha: int = 2, cached: bool = True):
+    def __init__(self, flow: str = "source_to_target", node_dim: int = 0, alpha: int = 2, normalize: bool = True, cached: bool = True):
         super(CBMSequentialTorchCSRSparseMatrixMessagePassing, self).__init__(flow, node_dim)
+        self.normalize = normalize
         self.cached = cached
         self.alpha = alpha
         self.cached_a_t = None
@@ -75,13 +77,13 @@ class CBMSequentialTorchCSRSparseMatrixMessagePassing(MessagePassing):
         if self.cached:
             if self.cached_a_t is None:
                 edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), alpha=self.alpha)
+                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
             a_t = self.cached_a_t
         else:
             edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), alpha=self.alpha)
+            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
 
-        out = a_t.seq_torch_csr_matmul(x)
+        out = a_t.seq_torch_csr_fused_matmul(x) if self.normalize else a_t.seq_torch_csr_matmul(x)
         return out
 
 
@@ -95,11 +97,11 @@ class CBMParallelTorchCSRSparseMatrixMessagePassing(CBMSequentialTorchCSRSparseM
         if self.cached:
             if self.cached_a_t is None:
                 edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), alpha=self.alpha)
+                self.cached_a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
             a_t = self.cached_a_t
         else:
             edge_index_t = stack([edge_index[1], edge_index[0]], dim=0).to(int32)
-            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), alpha=self.alpha)
+            a_t = cbm_matrix(edge_index_t, ones(edge_index.size(1), dtype=x.dtype), normalized=self.normalize, alpha=self.alpha)
 
-        out = a_t.omp_torch_csr_matmul(x)
+        out = a_t.omp_torch_csr_fused_matmul(x) if self.normalize else a_t.omp_torch_csr_matmul(x)
         return out
